04e915087d2916b425acc642ccbdf2385f3a337a
Professional <random9804@yandex.ru>
2022-03-20 23:29:21 +0600

    onViewCreated method for CounterFragment.kt

--- app/src/main/java/com/example/countermeiram/CounterFragment.kt
+++ app/src/main/java/com/example/countermeiram/CounterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,20 @@ class CounterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_counter, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        val counter = view.findViewById<TextView>(R.id.counter)
+        view.findViewById<Button>(R.id.plus).setOnClickListener{
+            var counterValue = counter.text.toString().toInt()
+            counter.text = (++counterValue).toString()
+        }
+        view.findViewById<Button>(R.id.minus).setOnClickListener{
+            var counterValue = counter.text.toString().toInt()
+            if (counterValue > 0)
+                counter.text = (--counterValue).toString()
+        }
     }
 
     companion object {
